5c281d2ee7389400a188ed822444f4c1df6f1f12
ramla199 <75176930+ramla199@users.noreply.github.com>
2021-10-13 16:00:57 +0100

    update 1

---
+++ 02-2-flex-items/js1-week1.js
@@ -0,0 +1,15 @@
+// console.log("Hello World!");
+// var greeting = "hello";
+// console.log(greeting);
+// console.log(greeting);
+// console.log(greeting);
+
+
+function club(firstName,lastName,yearJoined) {
+    var totalYear = 2021 - yearJoined;
+    console.log("Dear "+ firstName + " " + lastName + "thank you for being a member for  " +totalYear+ " years") ;
+  
+}
+club("john","snow",2020);
+club("arya","stark",2013);
+club("tywin","lannister",1987);
